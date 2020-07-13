ef4eb0a835569304bca6590029dd9450d02e1b7b
Fady Salama <fadytawfik11@gmail.com>
2020-07-13 10:59:32 +0200

    add MaGe specific components

---
+++ src/components/02_molecules/mFilterConstraintsAreaMaGe.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="filters-container">
+        <div class="header">
+            <label>Filters and Constraints</label>
+        </div>
+        <div class="body">
+            <div>
+                <h4>Allowed Types:</h4>
+                <div>
+                    <input type="checkbox"> <label>String</label>
+                </div>
+                <div>
+                    <input type="checkbox"> <label>Integer</label>
+                </div>
+                <div>
+                    <input type="checkbox"> <label>Number</label>
+                </div>
+                <div>
+                    <input type="checkbox"> <label>Boolean</label>
+                </div>
+                <div>
+                    <input type="checkbox"> <label>Array</label>
+                </div>
+                <div>
+                    <input type="checkbox"> <label>Object</label>
+                </div>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+    name: 'mFilterConstraintsAreaMaGe',
+    props: {
+        constraintsForm: {
+            type: Object,
+            required: false
+        }
+    }
+});
+</script>
+
+<style lang="less" scoped>
+.filters-container {
+    border: 0.5pt solid #393B3A;
+    border-radius: 3pt;
+}
+
+.header {
+    display: flex;
+    flex-flow: row nowrap;
+    align-items: center;
+    border-bottom: 0.5pt solid #393B3A;
+    padding: 5pt;
+}
+
+.body {
+    display: flex;
+    flex-flow: row nowrap;
+    background-color: #939C9E;
+    padding: 5pt;
+}
+
+
+</style>
---
+++ src/components/02_molecules/mTemplateSelectionAreaMaGe.vue
@@ -0,0 +1,62 @@
+<template>
+    <div id="selection-area-container">
+        <aTemplateSelectionMaGe
+        :title="'Subscription-driven'"
+        :imgSrc="'event-template'"
+
+        class="template-selection"
+        >
+            <template v-slot:desription>
+                The interaction sequence starts when the mashup agent receives an event. 
+                The data from the event is then sent to the matching outputs as input for an action or as a property write.
+            </template>
+        </aTemplateSelectionMaGe>
+
+        <aTemplateSelectionMaGe
+        :title="'Read-driven'"
+        :imgSrc="'read-template'"
+
+        class="template-selection"
+        >
+            <template v-slot:desription>
+                The interaction sequence starts with the mashup agent reading a property from an input device. 
+                The data is then sent to the matching outputs as input for an action or as a property write.
+            </template>
+        </aTemplateSelectionMaGe>
+
+        <aTemplateSelectionMaGe
+        :title="'Action-driven'"
+        :imgSrc="'action-template'"
+
+        class="template-selection"
+        >
+            <template v-slot:desription>
+                The interaction sequence starts with the mashup agent invoking an action that has an output from an input device. 
+                The received data is then sent to the matching outputs as input for an action or as a property write.
+            </template>    
+        </aTemplateSelectionMaGe>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import aTemplateSelectionMaGe from '@/components/01_atoms/aTemplateSelectionMaGe.vue'
+export default Vue.extend({
+    name: 'mTemplateSelectionAreaMaGe',
+    components: {
+        aTemplateSelectionMaGe
+    }
+})
+</script>
+
+<style lang="less" scoped>
+    #selection-area-container {
+        display: flex;
+        flex-flow: row nowrap;
+    }
+
+    .template-selection {
+        width: 32%;
+        height: 100%;
+    }
+</style>
